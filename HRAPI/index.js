a623efc4892c570677f019b618371929088d7363
SarimAyubi1 <bscs2312132@szabist.pk>
2025-05-18 14:02:21 +0000

    project

--- HRAPI/index.js
+++ HRAPI/index.js
@@ -25,6 +25,87 @@ app.get('/cat', async (req, res) => {
     }
 });
 
+app.get('/con', async (req, res) => {
+    try {
+        const result = await pool.query('select * from contracts');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ Error: err.message });
+    }
+});
+
+app.get('/fskill', async (req, res) => {
+    try {
+        const result = await pool.query('select * from freelancer_skills');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ Error: err.message });
+    }
+});
+
+app.get('/mess', async (req, res) => {
+    try {
+        const result = await pool.query('select * from messages');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ Error: err.message });
+    }
+});
+
+app.get('/pay', async (req, res) => {
+    try {
+        const result = await pool.query('select * from payments');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ Error: err.message });
+    }
+});
+
+app.get('/pro', async (req, res) => {
+    try {
+        const result = await pool.query('select * from projects');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ Error: err.message });
+    }
+});
+
+app.get('/prop', async (req, res) => {
+    try {
+        const result = await pool.query('select * from proposals');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ Error: err.message });
+    }
+});
+
+app.get('/rev', async (req, res) => {
+    try {
+        const result = await pool.query('select * from reviews');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ Error: err.message });
+    }
+});
+
+app.get('/ski', async (req, res) => {
+    try {
+        const result = await pool.query('select * from skills');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ Error: err.message });
+    }
+});
+
+app.get('/user', async (req, res) => {
+    try {
+        const result = await pool.query('select * from users');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ Error: err.message });
+    }
+});
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Connect Successfully... on PORT ${PORT}`);
